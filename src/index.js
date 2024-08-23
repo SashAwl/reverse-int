5cd1b471fd9bb0f9cd61fa9786a99b06a4106610
SashAwl <aleks.shilova@gmail.com>
2024-08-23 18:45:25 +0300

    feat: create solution for reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    s = 0;
+    n = Math.abs(n);
+    while (n > 0) {
+        s = s * 10 + n % 10;
+        n = Math.trunc(n / 10);
+    }
+    return s;
 }
